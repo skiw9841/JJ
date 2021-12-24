068961203e1a8092affd998ee69a21afe9ada935
sw <skiw9841@gmail.com>
2021-12-24 11:17:53 +0900

    Initial commit

---
+++ src/main/java/com/park/aa/JJ/JjApplication.java
@@ -0,0 +1,13 @@
+package com.park.aa.JJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/park/aa/JJ/JjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.aa.JJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
